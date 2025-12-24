3fe18f4d596a3af6b5366eda247c5733e779487a
HinohArata <mmgcntk@gmail.com>
2025-12-25 02:01:34 +0700

    run build with 32 jobs count
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- builder/build.sh
+++ builder/build.sh
@@ -94,7 +94,7 @@ fi
 
 # Start building
 echo "Starting make process with all available cores..."
-m afterlife -j48 || { 
+m afterlife -j32 || { 
     kill $PARSER_PID 2>/dev/null
     echo "Build failed"; exit 1; 
 }
